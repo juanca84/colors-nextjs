8c1c800df0bdefe1ca007b41f3917ac225aef6c3
Juan Carlos Condori <juanc.com@gmail.com>
2022-05-09 21:03:21 -0400

    feat: adicionando rutas dinamicas de un json y generando paginas dinamicas

---
+++ pages/[color].js
@@ -0,0 +1,23 @@
+import colors from "../data/colors.json";
+
+export const getStaticPaths = async () => {
+  const paths = colors.map((color) => ({
+    params: {
+      color: color.name,
+    },
+  }));
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const color = colors.find((color) => color.name === params.color);
+  return { props: { color } };
+};
+
+export default function Color({ color }) {
+  return (
+    <div className="color-page" style={{ backgroundColor: color.hex }}>
+      <h1>{color.name}</h1>
+    </div>
+  );
+}
